a6d36a527beb12c10836083c758c44d75c35296b
Alex M <alexey.m@horoshop.ua>
2025-09-25 01:18:43 +0300

    homework 19 added

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://guest:welcome2qauto@qauto.forstudy.space",
     pageLoadTimeout: 30000,
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 2000,
     video: true,
     screenshotOnRunFailure: true
 
